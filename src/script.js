c15c50661bb3c43f647bfea3a82cec3a7af1651a
dr0th3r <dr0th3r.code@gmail.com>
2023-11-20 21:39:10 +0100

    Add Ship class

---
+++ src/script.js
@@ -0,0 +1,15 @@
+class Ship {
+    constructor(length) {
+        this.length = length;
+        this.hitCount = 0;
+    }
+
+    isSunk() {
+        return this.hitCount >= this.length
+    }
+
+    hit() {
+        this.hitCount++;
+    }
+}
+
